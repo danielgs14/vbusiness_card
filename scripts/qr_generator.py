27ca27531cf3d03658e1706f34f77500980d4b2e
Daniel Gonzalez <danielgs14@gmail.com>
2024-05-30 17:10:19 -0600

    updated vcf and qr files

--- scripts/qr_generator.py
+++ scripts/qr_generator.py
@@ -1,2 +1,28 @@
 # This script will help you generate a png file containing a QR Code. 
 # The QR Code can be scanned to obtain a vCard.
+
+# imports
+import qrcode
+
+# Create the QR Code to be scanned
+qr = qrcode.QRCode(
+    version = 1
+    , error_correction = qrcode.constants.ERROR_CORRECT_L # this is a simple QR so should be able to be scanned easily
+    , box_size = 14 
+    , border = 2,
+)
+
+# perhaps it may be more tidy to specify here the colors to be used later
+bg = (0, 0, 0)
+fill = (255, 255, 255)
+
+# add your website here
+qr.add_data('https://<your_website.com>')
+
+# make it!
+img = qr.make_image(
+    back_color = bg
+    , fill_color = fill)
+
+# save it!
+img.save("./files/qr_code_website.png")
--- scripts/vcard_generator.py
+++ scripts/vcard_generator.py
@@ -1,8 +1,12 @@
-# This script will help you generate a vcf file containing your information. 
+# This script will help you generate a vcf file containing your information and then produce a png with it 
 
 # imports
+# imports for vCard
 from vobject import vCard
 from vobject.vcard import Name
+
+# imports for QR
+import qrcode
 
 # create contact info
 # you can add more attributes. check this link: https://en.wikipedia.org/wiki/VCard
@@ -26,7 +30,34 @@ vcard.useBegin = True
 for key, value in contact_me.items():
     vcard.add(key).value = value
 
-# Print and write the vCard to a file
-print(vcard.serialize())
+# store vCard in object
+qr_vcard = vcard.serialize()
+
+# write the vCard to a vcf file
 with open('./files/contact.vcf', 'w') as f:
     f.write(vcard.serialize())
+
+#### QR for vCard
+# let's create a qr with the vCard!
+
+qr = qrcode.QRCode(
+    version = 1
+    , error_correction = qrcode.constants.ERROR_CORRECT_L # this is a simple QR so error correction doesn't need to be high
+    , box_size = 14 
+    , border = 2,
+)
+
+# it may be more tidy to specify here the colors to be used later
+bg = (0, 0, 0)
+fill = (255, 255, 255)
+
+# add your vCard here
+qr.add_data(qr_vcard)
+
+# make it!
+img = qr.make_image(
+    back_color = bg
+    , fill_color = fill)
+
+# save it!
+img.save("./files/qr_vCard.png")
