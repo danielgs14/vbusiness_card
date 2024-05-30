a1f68877bf9357451c9a2f8aa3760015d1284999
Daniel Gonzalez <danielgs14@gmail.com>
2024-05-30 13:38:36 -0600

    updated vcard files

--- scripts/vcard_generator.py
+++ scripts/vcard_generator.py
@@ -1,19 +1,32 @@
-# This script will help you generate a png file containing a QR Code. 
-# The QR Code can be scanned to obtain a vCard.
+# This script will help you generate a vcf file containing your information. 
 
-#imports
-import qrcode 
-import vobject
+# imports
+from vobject import vCard
+from vobject.vcard import Name
 
-qr = qrcode.QRCode(
-    version = none,
-    error_correction = qrcode.constants.ERROR_CORRECT_L,
-    box_size = 10,
-    border = 4,
-)
-qr.add_data('Some data')
-qr.make(fit=True)
+# create contact info
+# you can add more attributes. check this link: https://en.wikipedia.org/wiki/VCard
+# remember to remove <>
 
-img = qr.make_image(fill_color="black", back_color="white")
+contact_me = {
+    'N': Name(family='<your_last_name> - <your_country>', given='<your_first_name>') # I'm going to a conference so I'm also adding my country for easy recalling
+    , 'FN': '<your_first_name> <your_last_name> - <your_country>' 
+    , 'TEL': '<+123456789>' #Remember to include the plus sign and country code
+    , 'EMAIL': '<your_email@email.com>'
+    , 'URL': '<your_website.com>'
+    , 'NOTE': 'Affiliated to <your_org>' # Using the ORG attribute returned semicolons so I'm using this instead
+}
 
-img = qr.make_image(back_color=(6, 107, 167), fill_color=(250, 186, 118))
+# create and being vCard
+vcard = vCard()
+vcard.name = 'vCard'
+vcard.useBegin = True
+
+# loop to use dictionary values
+for key, value in contact_me.items():
+    vcard.add(key).value = value
+
+# Print and write the vCard to a file
+print(vcard.serialize())
+with open('./files/contact.vcf', 'w') as f:
+    f.write(vcard.serialize())
